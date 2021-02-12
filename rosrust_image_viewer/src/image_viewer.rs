2e4bf4def91db480c3cfc4159e1099987998d5fd
Lucas Walter <wsacul@gmail.com>
2021-02-12 09:31:24 -0800

    Fixed resize, the setting of pixel data differently wasn't the solution, it was the suppression of the rate sleep.

--- rosrust_image_viewer/src/image_viewer.rs
+++ rosrust_image_viewer/src/image_viewer.rs
@@ -1,6 +1,6 @@
 mod utility;
 
-// use byteorder::{WriteBytesExt, BigEndian};
+use byteorder::{WriteBytesExt, BigEndian};
 use pixels::{Pixels, SurfaceTexture};
 use rosrust_msg::sensor_msgs::Image;
 // use rosrust::api::raii as ros;
@@ -11,7 +11,12 @@ use winit::event::{Event, VirtualKeyCode};
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit_input_helper::WinitInputHelper;
 
+pub fn from_rgb(r: u8, g: u8, b: u8) -> u32 {
+    (r as u32) << 24 | (g as u32) << 16 | (b as u32) << 8
+}
+
 fn main() {
+    rosrust::init("image_viewer");
     let screen_width: u32 = 800;
     let screen_height: u32 = 600;
     let event_loop = EventLoop::new();
@@ -19,8 +24,9 @@ fn main() {
     let (window, p_width, p_height, mut _hidpi_factor) =
         create_window("Image Viewer", screen_width, screen_height, &event_loop);
     let surface_texture = SurfaceTexture::new(p_width, p_height, &window);
+    rosrust::ros_info!("{} {}, {} {}", screen_width, screen_height, p_width, p_height);
 
-    // The compiler says this shouldn't be mut, but why not?  It's mut later
+    // TODO(lucasw) The compiler says this shouldn't be mut, but why not?  It's mut later
     // when it is extracted from the mutex.
     let pixels = Pixels::new(screen_width, screen_height, surface_texture).unwrap();
     let pixels_mutex = Mutex::new(pixels);
@@ -28,8 +34,6 @@ fn main() {
     let pixels_arc2 = pixels_arc1.clone();
 
     // let mut paused = false;
-
-    rosrust::init("image_viewer");
 
     let image_callback = {
         move |msg: Image| {
@@ -39,33 +43,33 @@ fn main() {
 
             // TODO(lucasw) check encoding
             let channels = 3;
-            for (count, pixel) in msg.data.iter().enumerate() {
-                let ind = count as u32 / channels;
-                let byte_ind = count as u32 - ind * channels;
-                let x = (ind / msg.width) as u32;
-                let y = (ind % msg.width) as u32;
-
-                if x >= screen_width {
+            for (count, mut pix) in screen.chunks_exact_mut(4).enumerate() {
+                let x = count as u32 % screen_width;
+                let y = count as u32 / screen_width;
+                if x >= msg.width {
                     continue;
                 }
-                if y >= screen_height {
-                    continue;
+                if y >= msg.height {
+                    break;
                 }
-
-                let remapped_byte_ind;
-                // change bgr to rgb
-                // TODO(lucasw) only if bgr
-                if byte_ind == 0 {
-                    remapped_byte_ind = 2;
-                } else if byte_ind == 2 {
-                    remapped_byte_ind = 0;
-                } else {
-                    remapped_byte_ind = 1;
+                let msg_byte_base_ind = (y * msg.width + x) as u32 * channels;
+                if (msg_byte_base_ind + 2) as usize >= msg.data.len() {
+                    eprintln!("{} {} -> {} {}, {} >= {}, msg {} x {}", count, screen_width, x, y,
+                              msg_byte_base_ind, msg.data.len(), msg.width, msg.height);
+                    break;
                 }
-
-                let viewer_ind = ((y * screen_width + x) * 4 + remapped_byte_ind) as usize;
-                screen[viewer_ind] = *pixel;
+                let msg_byte_base_ind = msg_byte_base_ind as usize;
+                let val = from_rgb(
+                    msg.data[msg_byte_base_ind + 2],
+                    msg.data[msg_byte_base_ind + 1],
+                    msg.data[msg_byte_base_ind + 0],
+                );
+                pix.write_u32::<BigEndian>(val).unwrap();
             }
+            // This skips the pixel buffer and write to the screen,
+            // which won't be scaled after a resize
+            // screen[viewer_ind] = *pixel;
+            rosrust::ros_info!("msg {} {}, frame {} {}", msg.width, msg.height, screen_width, screen_height);
         }
     };
 
@@ -102,11 +106,17 @@ fn main() {
             // Adjust high DPI factor
             if let Some(factor) = input.scale_factor_changed() {
                 _hidpi_factor = factor;
+                println!("scale factor changed {}", _hidpi_factor);
             }
             // Resize the window
             if let Some(size) = input.window_resized() {
                 let mut pixels_locked = Mutex::lock(&pixels_arc1).unwrap();
                 pixels_locked.resize(size.width, size.height);
+                println!("new width {} height {}", size.width, size.height);
+            }
+
+            if input.key_held(VirtualKeyCode::Left) || input.key_held(VirtualKeyCode::A) {
+                println!("left pressed");
             }
 
             window.request_redraw();
@@ -118,6 +128,7 @@ fn main() {
         }
 
         // thread::sleep(time::Duration::from_millis(33));
-        rate.sleep();
+        // This blocks winit inputs
+        // rate.sleep();
     }); // event_loop.run
 }
